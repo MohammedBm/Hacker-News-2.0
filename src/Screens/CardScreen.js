3664ca16921a51c5c0a9eeafa5fbb24240263c50
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-10-31 16:12:58 -0700

    - Initial commit

---
+++ src/Screens/CardScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, TouchableHighlight } from 'react-native'
+
+const CardScreen = ({ onPress, children, bigShadow }) => {
+  return(
+    <TouchableHighlight>
+      
+    </TouchableHighlight>
+  )
+}
