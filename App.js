9b7d9e4016f39749fcccaa7236ffcb4abf53f249
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-11-01 18:55:10 -0700

    - update App.js  - update App.json  - update Package.json

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import Story from "./src/Screens/Story";
 import Headlines from "./src/Screens/HeadlinesScreen";
 import storiesStore from "./src/Store/Stories";
-import AppStatusBar from "./src/Config/StatusBar";
 import MyScreen from "./src/Screens/MyScreen";
 
 export default class App extends React.Component {
@@ -12,7 +11,6 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        {/* <AppStatusBar /> */}
         <Route />
       </View>
     )
